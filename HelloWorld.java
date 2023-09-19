f7142f2519a5e61388db6f611168f46da28a6839
KFrancis25 <145369831+KFrancis25@users.noreply.github.com>
2023-09-19 09:15:18 +0100

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args){
+      //  System.out.println("%s%n%s%n", "Hello", "World");
+      //  System.out.println("World");
+      String hello = "Hello\nWorld\n";
+      System.out.print(hello);
+    }
+}
