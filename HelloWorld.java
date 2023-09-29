ba0c306cd778e3b22e6773ca6d433e6be3ef47f9
KAI FRANCIS <k.francis.23@abdn.ac.uk>
2023-09-29 13:32:08 +0000

    git

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args){
-      //  System.out.println("%s%n%s%n", "Hello", "World");
-      //  System.out.println("World");
       String hello = "Hello\nWorld\n";
       System.out.print(hello);
     }
